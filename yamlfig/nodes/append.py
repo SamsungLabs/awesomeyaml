e7f012b21356f959ce9ead89ab13185e9c842d3c
Łukasz Dudziak <l.dudziak@samsung.com>
2022-05-05 17:02:28 +0100

    [Append] Fix case when there is nothing to append to
    Per the current docs: fallback to be a normal ConfigList node

--- yamlfig/nodes/append.py
+++ yamlfig/nodes/append.py
@@ -32,6 +32,9 @@ class AppendNode(ConfigList):
 
     @namespace('yamlfigns')
     def on_premerge(self, path, into):
+        if into is None:
+            return ConfigList(self)
+
         node = into.yamlfigns.remove_node(path)
         if node is None:
             raise KeyError(f'Node {str(self)!r} does not exist in the previous context (possibly deleted?) - while processing a {type(self).__name__!r} node at {path!r}')
