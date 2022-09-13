518137013a75a3596db640d1da49e8d9d4e97e90
Łukasz Dudziak <vaenyr@gmail.com>
2022-09-13 16:44:57 +0200

    [Misc] Do not keep _dist_info file after building

--- setup.py
+++ setup.py
@@ -32,7 +32,9 @@ class build_maybe_inplace(build_py):
         _dist_file = version_file.parent.joinpath('_dist_info.py')
         assert not _dist_file.exists()
         _dist_file.write_text('\n'.join(map(lambda attr_name: attr_name+' = '+repr(getattr(package_version, attr_name)), package_version.__all__)) + '\n')
-        return super().run()
+        ret = super().run()
+        _dist_file.unlink()
+        return ret
 
 with Path(__file__).parent.joinpath('README.md').open('r') as f:
     long_desc = f.read()
