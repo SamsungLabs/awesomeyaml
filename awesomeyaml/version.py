827b83781796783286d7fef4bef29076df52b35e
Łukasz Dudziak <l.dudziak@samsung.com>
2023-03-30 17:19:21 +0100

    [Misc] Version 1.1.0.post1 (versioning-related fixes)

--- awesomeyaml/version.py
+++ awesomeyaml/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '1.1.0'
+version = '1.1.0.post1'
 repo = 'unknown'
 commit = 'unknown'
 has_repo = False
@@ -31,20 +31,31 @@ try:
             repo = r.remotes.origin.url
 
         commit = r.head.commit.hexsha
+        status = []
         if r.is_dirty():
-            commit += ' (dirty)'
+            status.append('dirty')
+        if r.untracked_files:
+            status.append(f'+{len(r.untracked_files)} untracked')
+        if status:
+            commit += f' ({",".join(status)})'
     except git.InvalidGitRepositoryError:
         raise ImportError()
 except ImportError:
     pass
 
 try:
-    from . import _dist_info as info
+    from . import _dist_info as _info
     assert not has_repo, '_dist_info should not exist when repo is in place'
-    assert version == info.version
-    repo = info.repo
-    commit = info.commit
+    assert version == _info.version
+    repo = _info.repo
+    commit = _info.commit
 except (ImportError, SystemError):
     pass
+
+
+def info():
+    g = globals()
+    return { k: g[k] for k in __all__ }
+
 
 __all__ = ['version', 'repo', 'commit', 'has_repo']
